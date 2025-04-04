e6a511e13f3b7abdf36cc6e94b53947e17ac3ade
Lucahubgit <lucaandri2000@gmail.com>
2025-04-04 10:13:02 +0000

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,26 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# METRIC
+st.title("Metric")
+
+st.metric(label="Number of assists", value="25", delta="+6")
+
+st.metric(label="Goals", value="13", delta="-5")
+
+# DATAFRAME
+st.title(":blue[Dataframe]")
+
+data={
+    "Player's name": ["Pelè", "Maradona", "Baggio"],
+    "Goals": [13, 25, 24],
+    "Team": ["Juve", "Napoli", "Inter"]
+}
+
+df=pd.DataFrame(data)
+
+st.subheader("Dataframe")
+st.dataframe(df)    #to display the dataframe
+
+st.subheader("Static table")    #static means that you can't interact with the table
+st.table(df)
